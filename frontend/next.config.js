9f120c0671857efa373475d48d37baf3066fcd7d
Lioz2468 <lioz2468@gmail.com>
2026-03-16 20:20:17 +0200

    fix next config syntax

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+module.exports = nextConfig;
